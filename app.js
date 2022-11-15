5d3e8b8dc1059041eaeabda05f14d9f3040c7d11
beltranaugusto <beltranliscano@gmail.com>
2022-11-15 14:23:12 -0400

    Completed the v1 of the project

---
+++ app.js
@@ -0,0 +1,17 @@
+let who = ['The dog','My grandma','His turtle','My bird'];
+let action = ['ate','peed','crushed','broke'];
+let what = ['my homework', 'the keys', 'the car'];
+let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+
+document.querySelector("#excuse").innerHTML = `
+${who[Math.floor(Math.random()*who.length)]} 
+${action[Math.floor(Math.random()*action.length)]} 
+${what[Math.floor(Math.random()*what.length)]} 
+${when[Math.floor(Math.random()*when.length)]}
+`
+
+document.querySelector("body").style.height = "100vh"
+document.querySelector("body").style.display = "flex"
+document.querySelector("body").style.flexDirection = "column"
+document.querySelector("body").style.alignItems = "center"
+document.querySelector("body").style.justifyContent = "center"
